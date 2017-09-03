29bc214d23a015c87961455674aefc52f4bfe5e4
szalaiz <szalai.reg@gmail.com>
2017-09-04 00:53:16 +0200

    Revert

--- decide.php
+++ decide.php
@@ -33,60 +33,6 @@
 </HEAD>
 
 <BODY>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '486977988349508',
-      xfbml      : true,
-      version    : 'v2.10'
-    });
-    FB.AppEvents.logPageView();
-  };
-
-  
-  
-  function onLogin(response) {
-  if (response.status == 'connected') {
-    FB.api('/me?fields=first_name', function(data) {
-      var welcomeBlock = document.getElementById('fb-welcome');
-      welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
-    });
-  }
-}
-
-FB.getLoginStatus(function(response) {
-  // Check login status on load, and if the user is
-  // already logged in, go directly to the welcome message.
-  if (response.status == 'connected') {
-    onLogin(response);
-  } else {
-    // Otherwise, show Login dialog first.
-    FB.login(function(response) {
-      onLogin(response);
-    }, {scope: 'user_friends, email'});
-  }
-});
-  
-  
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
-
-  <h1 id="fb-welcome"></h1>
-  
-  
-<div
-  class="fb-like"
-  data-share="true"
-  data-width="450"
-  data-show-faces="true">
-</div>
-
   <DIV id="box1"></DIV>
   <DIV id="box2"></DIV>
   <DIV id="box3"></DIV>
